55c7992d6111968ad073c8408b3b4adb53fd8c46
mengmengep-arch <mengmeng.ep@gmail.com>
2025-10-01 20:04:03 +0700

    Change name

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # this is a simple Python script that prints "Hello World" and a name to the console.
 def helloworld():
     print('Hello World')
-    print('My Name is Eka')
+    print('My Name is Ekaluk')
 
 helloworld()
