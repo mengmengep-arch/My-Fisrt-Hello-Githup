75f1cc68462477026a9bb7335eaf1b89597679f0
mengmengep-arch <mengmeng.ep@gmail.com>
2025-10-01 20:00:49 +0700

    move hello world to function

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-print('Hello World')
-print('My Name is Eka')
+# this is a simple Python script that prints "Hello World" and a name to the console.
+def helloworld():
+    print('Hello World')
+    print('My Name is Eka')
+
+helloworld()
